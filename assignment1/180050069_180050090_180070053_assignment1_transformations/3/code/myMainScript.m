26025d733bea46722385b963df579588613fd91a
Niraj Mahajan <nirajmahajan007@gmail.com>
2020-08-28 03:18:37 +0530

    A1 Q3 code done

--- assignment1/180050069_180050090_180070053_assignment1_transformations/3/code/myMainScript.m
+++ assignment1/180050069_180050090_180070053_assignment1_transformations/3/code/myMainScript.m
@@ -1,6 +1,5 @@
-%% MyMainScript
-
-tic;
-%% Your code here
-
-toc;
+clear;
+close all;
+clc;
+myplot('../data/man.jpeg', '../images/man.png', 1);
+myplot('../data/statue.png', '../images/statue.png', 2);
