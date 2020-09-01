5ed535758f813648bcea8e337debf48692997af9
Niraj Mahajan <nirajmahajan007@gmail.com>
2020-09-01 23:51:54 +0530

    Samarth: corrected q2 part D

--- assignment1/180050069_180050090_180070053_assignment1_transformations/2/code/myHM.m
+++ assignment1/180050069_180050090_180070053_assignment1_transformations/2/code/myHM.m
@@ -1,32 +1,24 @@
 function myHM(img,imgMask,ref,refMask)
     A = imread(img);
     A_mask = imread(imgMask);
-    A_masked = A;
     B = imread(ref);
     B_mask = imread(refMask);
-    B_masked = B;
     [rows,columns,colors] = size(A);
     out_eq = zeros(size(A));
     out_hm = zeros(size(A));
     for i = 1:colors
-        for it = 1:rows
-            for jt = 1:columns
-                if A_mask(it,jt)==0
-                    A_masked(it,jt,i)=0;
-                end
-                if B_mask(it,jt)==0
-                    B_masked(it,jt,i)=0;
-                end
-            end
-        end
-        layer_a = A_masked(:,:,i);
-        layer_b = B_masked(:,:,i);
+        layer_a = A(:,:,i);
+        layer_b = B(:,:,i);
         freq_a = zeros(256);
         freq_b = zeros(256);
         for it = 1:rows
             for jt = 1:columns
-                freq_a(layer_a(it,jt)+1)= freq_a(layer_a(it,jt)+1)+1;
-                freq_b(layer_b(it,jt)+1)= freq_b(layer_b(it,jt)+1)+1;
+                if A_mask(it,jt)>0
+                    freq_a(layer_a(it,jt)+1)= freq_a(layer_a(it,jt)+1)+1;
+                end
+                if B_mask(it,jt)>0
+                    freq_b(layer_b(it,jt)+1)= freq_b(layer_b(it,jt)+1)+1;
+                end
             end
         end
         hist_a = zeros(256);
@@ -51,11 +43,20 @@ function myHM(img,imgMask,ref,refMask)
                 end
             end
         end
-        out_eq(:,:,i) = arrayfun(@(x) round((double(hist_a(x+1))/cdf_a)*255),A(:,:,i));
-        out_hm(:,:,i) = arrayfun(@(x) map_ab(x+1),A(:,:,i));
+        for it=1:rows
+            for jt=1:columns
+                if A_mask(it,jt)>0
+                    out_eq(it,jt,i)=round((double(hist_a(A(it,jt,i)+1))/cdf_a)*255);
+                    out_hm(it,jt,i)=map_ab(A(it,jt,i)+1);
+                else 
+                    out_eq(it,jt,i)=A(it,jt,i);
+                    out_hm(it,jt,i)=A(it,jt,i);
+                end
+            end
+        end
     end
     subplot(1,3,1), imshow(A), title('original')
     subplot(1,3,2), imshow(mat2gray(out_eq)), title('after HE')
     subplot(1,3,3), imshow(mat2gray(out_hm)), title('after HM')
-%     save('../images/d/d4.mat','out_hm');
+%      save('../images/d/d4.mat','out_hm');
 end
