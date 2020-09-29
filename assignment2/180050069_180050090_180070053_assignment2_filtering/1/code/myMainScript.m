c2e6d6f256c0382a9a29dda8ed992d8446e29747
SamarthSingh13 <samsin1372@gmail.com>
2020-09-29 11:50:52 +0530

    question 2

--- assignment2/180050069_180050090_180070053_assignment2_filtering/1/code/myMainScript.m
+++ assignment2/180050069_180050090_180070053_assignment2_filtering/1/code/myMainScript.m
@@ -2,5 +2,7 @@
 
 tic;
 %% Your code here
+figure('Name','Sharpening on superMoonCrop.mat'), myUnsharpMasking('../data/superMoonCrop.mat')
+figure('Name','Sharpening on lionCrop.mat'), myUnsharpMasking('../data/lionCrop.mat')
 
 toc;
