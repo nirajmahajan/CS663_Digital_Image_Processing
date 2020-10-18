b2455016f77807e12f297ebade1381989a2cda93
Niraj Mahajan <nirajmahajan007@gmail.com>
2020-10-18 21:35:43 +0530

    Final Submission added A3

--- assignment3/180050069_180050090_180070053_assignment2_segmentation/3/code/myMainScript.m
+++ assignment3/180050069_180050090_180070053_assignment2_segmentation/3/code/myMainScript.m
@@ -16,11 +16,4 @@ cutoff = 40;
 gaussian_sigma = 0.7;
 workOnImage(I, 'bird', 2, hi, lo, gaussian_sigma, cutoff);
 
-f = figure(23);
-imshow(I);
-title('Original Image')
-saveas(f, sprintf('../images/%s_original.png',name));
-pause(0.2);
-savefig(sprintf('../images/figs/%s_original.fig',name));
-
 toc;
