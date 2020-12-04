4c30711c31a4e679d84c493a3ac041a785660680
Niraj Mahajan <nirajmahajan007@gmail.com>
2020-12-04 21:50:01 +0530

    added fld class

--- project/code/classes.py
+++ project/code/classes.py
@@ -38,3 +38,56 @@ class PCA(object):
 		return -(X - self.mean.reshape(1,-1))@self.transform_mat
 
 
+class FLD(object):
+	"""An object for computing the fischer linear discriminant of the input array"""
+	def __init__(self, out_dim = 1):
+		super(FLD, self).__init__()
+		self.out_dim = out_dim
+
+	# X 	 -> nxd
+	# labels -> n,
+	# Assuming labels of the form {0,1,2,3...c-1}
+	def fit(self, X, labels):
+		self.in_dim = X.shape[1]
+		self.n_data = X.shape[0]
+		self.c = labels.max()+1
+		# error check
+		for i in range(self.c):
+			if not i in labels:
+				print("Class {} not present in labels".format(i), flush = True)
+				os._exit(-1)
+
+		# generate classwise X and number of elements per class (Ni)
+		classwise_X = []
+		MUi = []
+		Ni = []
+		for i in range(self.c):
+			indices = labels == c
+			classwise_X.append(X[indices,:])
+			Ni.append(indices.sum())
+			MUi.append(classwise_X[i].mean(0).reshape(-1,1))
+
+		# generate Sb, Sw
+		self.mu_global = X.mean(0).reshape(-1,1)
+		self.Sb = np.zeros((self.in_dim,self.in_dim))
+		self.Sw = np.zeros((self.in_dim,self.in_dim))
+		for i in range(self.c):
+			vec = MUi[i] - self.mu_global
+			Xi_norm = classwise_X[i] - MUi[i].reshape(1,-1)
+			self.Sb += Ni[i]*(vec@vec.T)
+			self.Sw += (Xi_norm.T@Xi_norm)
+
+		# get the generalised eigvals
+		self.eigvals_gen, self.W = scipy.linalg.eigh(Sb,Sw,eigvals_only=False)
+
+		indices = np.argsort(self.eigvals_gen)[::-1]
+		self.eigvals_gen_all = self.eigvals_gen[indices]
+		indices = indices[:self.out_dim]
+		self.eigvals_gen = self.eigvals_gen[indices]
+		self.W = self.W[:,indices]
+
+	def transform(self, X):
+		return X@self.W
+
+
+		
