537855fd817d98999353893863fed4add8d6895e
Niraj Mahajan <nirajmahajan007@gmail.com>
2020-12-06 01:10:20 +0530

    added helpers in utils.py

--- project/code/utils.py
+++ project/code/utils.py
@@ -1,5 +1,53 @@
 import numpy as np
 import scipy
+import sklearn
+from sklearn.decomposition import PCA as PP
 import matplotlib.pyplot as plt
 import os
+import argparse
+import sys
+import time
+import random
+
+from classes import FischerfacePredictor,EigenfacePredictor,EigenfacePredictorIllum
+
+def getAllErrors(Xtrain, Ytrain, Xtest, Ytest, parameters = [15,30,30]):
+	fischer_model = FischerfacePredictor(parameters[0])
+	fischer_model.train(Xtrain, Ytrain)
+
+	prediction = fischer_model.test(Xtest)
+	fischer_accuracy = (prediction == Ytest).sum()/Ytest.shape[0]
+
+	eigen_model = EigenfacePredictor(parameters[1])
+	eigen_model.train(Xtrain, Ytrain)
+
+	prediction = eigen_model.test(Xtest)
+	eigen_accuracy = (prediction == Ytest).sum()/Ytest.shape[0]
+
+	eigen_illu_model = EigenfacePredictorIllum(parameters[2])
+	eigen_illu_model.train(Xtrain, Ytrain)
+
+	prediction = eigen_illu_model.test(Xtest)
+	eigen_illu_accuracy = (prediction == Ytest).sum()/Ytest.shape[0]
+
+	return 1-fischer_accuracy, 1-eigen_accuracy, 1-eigen_illu_accuracy
+
+# to be used for yale, cmu, not yaleB
+def getAllErrorsLeavingOne(X, Y, parameters = [1,10,10]):
+	fischer_error = 0
+	eigen_error = 0
+	eigen_illu_error = 0
+	for i in range(Y.shape[0]):
+		tempX = np.delete(X,i,0)
+		tempY = np.delete(Y,i,0)
+		fe, ee, eie = getAllErrors(tempX, tempY, X[i,:].reshape(1,-1), np.array([Y[i]]), parameters = parameters)
+		fischer_error += fe
+		eigen_error += ee
+		eigen_illu_error += eie
+
+	fischer_error /= Y.shape[0]
+	eigen_error /= Y.shape[0]
+	eigen_illu_error /= Y.shape[0]
+
+	return fischer_error, eigen_error, eigen_illu_error
 
